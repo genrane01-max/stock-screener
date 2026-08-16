a0cb38ef0f827426649a766d3ba4286f76eb51cb
genrane01-max <genrane01@gmail.com>
2026-08-16 07:14:03 +0700

    Update server.js

--- server.js
+++ server.js
@@ -10,14 +10,25 @@ app.use(express.json());
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-// แสดงหน้าเว็บเมื่อเปิด URL หลัก
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
+// ⭐ รายชื่อหุ้น SET50 + SET100 (70 ตัว)
 const SET_TICKERS = [
-  "PTT.BK", "ADVANC.BK", "KBANK.BK", "CPALL.BK",
-  "SCB.BK", "AOT.BK", "GULF.BK", "DELTA.BK"
+  // SET50
+  "ADVANC.BK","AOT.BK","AWC.BK","BANPU.BK","BBL.BK","BCP.BK","BDMS.BK",
+  "BEM.BK","BH.BK","BJC.BK","BTS.BK","CBG.BK","CCET.BK","COM7.BK",
+  "CPALL.BK","CPF.BK","CPN.BK","CRC.BK","DELTA.BK","EGCO.BK","GPSC.BK",
+  "GULF.BK","HMPRO.BK","IVL.BK","KBANK.BK","KCE.BK","KKP.BK","KTB.BK",
+  "KTC.BK","LH.BK","MINT.BK","MTC.BK","OR.BK","OSP.BK","PTT.BK",
+  "PTTEP.BK","PTTGC.BK","RATCH.BK","SCB.BK","SCC.BK","SCGP.BK","TCAP.BK",
+  "TIDLOR.BK","TISCO.BK","TLI.BK","TOP.BK","TRUE.BK","TTB.BK","TU.BK",
+  "VGI.BK","WHA.BK",
+  // SET100 เพิ่มเติม
+  "BA.BK","BCH.BK","BGRIM.BK","CK.BK","CKP.BK","EA.BK","ERW.BK",
+  "GLOBAL.BK","HANA.BK","JMT.BK","QH.BK","SAWAD.BK","SIRI.BK","SPALI.BK",
+  "STA.BK","STGT.BK","TASCO.BK","TOA.BK","TPIPL.BK"
 ];
 
 app.get("/api/stocks", async (req, res) => {
